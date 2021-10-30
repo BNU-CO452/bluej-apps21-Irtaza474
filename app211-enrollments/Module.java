ff2349e70add62fa5cb15fa1dbd9ae2d66897586
Irtaza Qayyum <22136251@bucks.ac.uk>
2021-10-30 19:13:12 +0100

    update module.java
    not finsihed yet

--- app211-enrollments/Module.java
+++ app211-enrollments/Module.java
@@ -1,76 +1,76 @@
 
+// Created by Irtaza Qayyum
+
 /**
- * This is a class module and is apart of the course.
- *
- * @author Irtaza Qayyum
- * @version 1.0
- */
-public class Module
-{
+ * This is a class module and is apart of the course.
+ *
+ * @author Irtaza Qayyum
+ * @version 1.0
+ */
+public class Module {
     // variable
     private String code;
     private String title;
-    private int credits;
-
+    private int credit;
+    
     /**
-     * Constructor for objects of class Module
-     */
-    public Module(String code,String title)
-    {
+     * Constructor for objects of class Module
+     */
+    public Module(String code, String title) {
         // initialise instance variables
         this.code = code;
         this.title = title;
         credit = 0;
-        
     }
-
+    
     /**
-     * The method will printout the module code and
-     * title
-     */
-    public String getCode()
-    {
-      return this.code;
-      
-   /**
-    * An example of a method -replace with yours.
+     * The method will printout the module code and
+     * title
+     */
+    public String getCode() {
+        return code;
+    }
+    
+    /**
+    * This method will return the title.
     * @param
     * @return code
     */
-   public
-   String getTitle();
-   {
-        return this.title;
-   }
-   
-   
-   public int getCredit()
-   {
-       return this.credit;
-   }
-      
-    public void setCredit(int credit)
-    {
+    public String getTitle() {
+        return title;
+    }
+
+    public int getCredit() {
+        return this.credit;
+    }
+
+    public void setCredit(int credit) {
         this.credit = credit;
-        
-        
+    }
+
+    /**
+    * The method will printout the module code and
+    * title
+    */
+    public void print() {
+        //printHeading();
+        System.out.println("Module Code: " + getCode() + " : " + getTitle());
+        System.out.println();
     }
     
-    /**
-     * The method will printout the module code and
-     * title
-    */
-    public void print()
-  {
-        //printHeading();
-    
-    System.out.printIn(" Module Code: " + code + ": " + title);
-    System.out.printIn();  
-  }
-   
+    public static void main(String[] args) {
+        Module m = new Module("abc", "def");
+        m.print();
+    }
 }
     
     
     
         
+    
+    
+        
       
+
+
+   
